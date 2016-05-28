19e19449d0f42b7cf86dc9415cdf6923b6975bd7
memo <me@memo.blue>
2016-05-27 20:47:43 -0400

    prep repo for Heroku

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 3000; // node
+
+// openweathermap api doesn't support httpS so we redirect everything to http
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('express server running');
+app.listen(PORT, function() {
+  console.log('Express server running on port: ' + PORT);
 });
