1728d2ef64bad2d109ad8aafb628d1333807ec75
memo <me@memo.blue>
2016-05-30 12:06:52 -0400

    style nav and make the nav comment insto a class component

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,15 +1,43 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = () => {
-  return (
-    <div>
-      <h2>Nav</h2>
-      <IndexLink activeClassName="active" to="/">Get Weather</IndexLink>
-      <Link activeClassName="active" to="/about">About</Link>
-      <Link activeClassName="active" to="/examples">Examples</Link>
-    </div>
-  );
-};
+var Nav = React.createClass({
+  onSearch: function(e) {
+    e.preventDefault();
+    console.log('not implemented yet');
+  },
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather</li>
+            <li>
+              <IndexLink activeClassName="active" to="/">Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link activeClassName="active" to="/about">About</Link>
+            </li>
+            <li>
+              <Link activeClassName="active" to="/examples">Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather" />
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather" />
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
 
 module.exports = Nav;
