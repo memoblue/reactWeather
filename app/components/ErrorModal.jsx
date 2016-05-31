a12a314a582297358ec379b806bf774784dda39d
memo <me@memo.blue>
2016-05-30 20:17:36 -0400

    reverted to my own version of the file after trouble shooting the error

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,31 +1,27 @@
 var React = require('react');
 
 var ErrorModal = React.createClass({
-  getDefaultProps: function () {
+  getDefaultProps: function() {
     return {
       title: 'Error'
     };
   },
   propTypes: {
-      title: React.PropTypes.string,
-      msg: React.PropTypes.string.isRequired
+    title: React.PropTypes.string,
+    msg: React.PropTypes.string.isRequired
   },
-  componentDidMount: function () {
+  componentDidMount: function() {
     var modal = new Foundation.Reveal($('#error-modal'));
     modal.open();
   },
-  render: function () {
-    var {title, msg} = this.props;
+  render: function() {
+    var { title, msg } = this.props;
 
     return (
-      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+      <div className="reveal tiny text-center" id="error-modal" data-reveal="">
         <h4>{title}</h4>
         <p>{msg}</p>
-        <p>
-          <button className="button hollow" data-close="">
-            Okay
-          </button>
-        </p>
+        <button className="button hollow" data-close="">Okay</button>
       </div>
     );
   }
